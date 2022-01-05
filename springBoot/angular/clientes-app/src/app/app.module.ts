664ca718ec4907ea9b913c33f412c3c8d383be29
Diego Armando Moya <85351327+damoyaweb@users.noreply.github.com>
2022-01-05 17:21:15 -0500

    Segundo Commit

--- springBoot/angular/clientes-app/src/app/app.module.ts
+++ springBoot/angular/clientes-app/src/app/app.module.ts
@@ -6,20 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
   ],
-  providers: [],
+  providers: [
+    ClienteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
