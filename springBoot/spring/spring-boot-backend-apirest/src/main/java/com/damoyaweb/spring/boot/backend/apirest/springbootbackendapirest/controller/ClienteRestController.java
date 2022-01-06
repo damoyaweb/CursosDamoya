fd9b9d1f888ac641ecdeedc47221e7a84274de4e
Diego Armando Moya <85351327+damoyaweb@users.noreply.github.com>
2022-01-06 17:25:23 -0500

    mi tercer commit

--- springBoot/spring/spring-boot-backend-apirest/src/main/java/com/damoyaweb/spring/boot/backend/apirest/springbootbackendapirest/controller/ClienteRestController.java
+++ springBoot/spring/spring-boot-backend-apirest/src/main/java/com/damoyaweb/spring/boot/backend/apirest/springbootbackendapirest/controller/ClienteRestController.java
@@ -6,10 +6,12 @@ import com.damoyaweb.spring.boot.backend.apirest.springbootbackendapirest.models
 import com.damoyaweb.spring.boot.backend.apirest.springbootbackendapirest.models.services.IClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:42000"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
